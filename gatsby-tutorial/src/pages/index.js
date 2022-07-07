fe2fbc63aa0df07191657bb9ba9ec57732c6b1ef
EricRicketts <eric_ricketts@me.com>
2022-07-07 09:52:49 -0400

    make images available from local file system

--- gatsby-tutorial/src/pages/index.js
+++ gatsby-tutorial/src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react"
 import Layout from "../components/layout";
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
   return (
     <Layout pageTitle={"Home Page"} >
       <p>I am making this page by following the Gatsby tutorial.</p>
+      <StaticImage
+        src="../images/clifford.jpg"
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera">
+      </StaticImage>
     </Layout>
   );
 }
